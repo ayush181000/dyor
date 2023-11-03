0fa3a4070553e11e565e28c72ef43dcb02f3333f
Ayush Garg <ayush181000@gmail.com>
2023-11-03 23:38:01 +0530

    remove auth fix cron

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -4,6 +4,6 @@ const { protect } = require("../controllers/authController");
 const { homepage, dashboard } = require("../controllers/dataController");
 
 router.get('/homepage', homepage);
-router.get('/dashboard', protect, dashboard);
+router.get('/dashboard', dashboard);
 
 module.exports = router;
