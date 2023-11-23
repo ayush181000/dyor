644a26463f7c3aa9fe56c298da7b2cc33aeb76cd
Ayush Garg <ayush181000@gmail.com>
2023-11-24 03:28:11 +0530

    update tokendata schema

--- models/tokenData.js
+++ models/tokenData.js
@@ -3,18 +3,24 @@ const mongoose = require('mongoose');
 const tokenDataSchema = mongoose.Schema({
     tokenName: { type: String, required: true },
     date: { type: Date },
-    price: Number,
     tvl: Number,
-    ttv: Number,
-    ttvChangePercentages: String,
+
     fdv: Number,
     holders: Number,
     activeHolders: Number,
+
+
+    totalSupply: Number,
+    daily_fee: Number,
+
+    price: Number,
+    priceChangeP: String,
+
+    ttv: Number,
+    ttvChangePercentages: String,
+
     circulatingSupply: Number,
     circulatingSupplyChangePercentages: String,
-    totalSupply: Number,
-    active_users: Number,
-    daily_fee: Number,
 
     stakingRatio: Number,
     stakingRatioChangePercentages: String
